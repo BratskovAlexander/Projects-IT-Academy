ebb4b29ad2962632a3ed30e9c765632acb3fbf0b
ВашÐBratskov Alexander <monutor@mail.ru>
2019-06-08 15:15:54 +0300

    added 11.1 but with bags

--- 11.1/src/modules/main.js
+++ 11.1/src/modules/main.js
@@ -1,3 +1,9 @@
+const htmlElements = {};
+htmlElements.button = document.querySelector('button');
+
+htmlElements.button.addEventListener("click", onButtonClick);
+
+
 function onButtonClick() {
     if (checkPassword() === true) {
         confirm('Спасибо, вы ввели верный пароль');
