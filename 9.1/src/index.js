082ac89f27f4bfd66b0afc20db3fd8e9801e492e
ВашÐBratskov Alexander <monutor@mail.ru>
2019-06-08 14:41:04 +0300

    added modules main.js

--- 9.1/src/index.js
+++ 9.1/src/index.js
@@ -1,21 +1,2 @@
-import { Room57 } from "./modules/Room57.js";
-
-
-const room57 = new Room57(57, 12, 3, 12);//её область видимости ограничена модулем index.js для него она глобальная
-
-room57.table.locate(true);
-room57.table.checkWhoseTable('table for a teacher');
-room57.projector.show(true, false);
-room57.projector.checkStateProjector('sleeping');
-room57.airCondition.blow(16, 25);
-room57.airCondition.checkNamecondition('Siemens');
-room57.table.laptop.open(true);
-room57.table.laptop.checkQuantityButton(104);
-room57.table.laptop.monitor.showMovie(true);
-room57.table.laptop.monitor.checkStateMonitor('sleeping');
-
-
-
-console.log(room57);
-
-
+import './style.css';
+import './modules/main.js';
