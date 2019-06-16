b3932dc804ed6b2e81eabafc68b2a2b832c5b781
ВашÐBratskov Alexander <monutor@mail.ru>
2019-06-16 23:03:07 +0300

    deleted comments

--- 12.2/src/modules/ClassHelper.js
+++ 12.2/src/modules/ClassHelper.js
@@ -15,19 +15,3 @@ ClassHelper.removeClass = removeClass;
 ClassHelper.addClass = addClass;
 
 export { addClass, removeClass, ClassHelper };
-
-// function addClass(className, elements) {
-//     for (let i = 0; i < elements.length; i++) {
-//         elements[i].classList.add(className);
-//     }
-// }
-
-// function removeClass(className, elements) {
-//     elements.forEach(function (element) {
-//         element.classList.remove(className);
-//     });
-// }
-
-// function ClassHelper() { }
-// ClassHelper.addClass = addClass;
-// ClassHelper.removeClass = removeClass;
