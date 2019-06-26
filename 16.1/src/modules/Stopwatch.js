721a0d1815f5676d9fd05720544fa779b33dcb81
Alexander Bratskov <AlexanderBratskov@localhost.localdomain>
2019-06-26 13:54:49 +0300

    added start to 16.1

---
+++ 16.1/src/modules/Stopwatch.js
@@ -0,0 +1,13 @@
+import { StopwatchTimer } from "./StopwatchTimer.js";
+
+function Stopwatch() {
+  StopwatchTimer.apply(this, ["stopwatch", 0]);
+}
+
+Stopwatch.prototype = Object.create(StopwatchTimer.prototype);
+
+Stopwatch.prototype.showInfo = function () {
+  console.log(this);
+};
+
+export { Stopwatch };
