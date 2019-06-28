d4a495683010bedfcd270487eb2995a04d1a9e30
Alexander Bratskov <AlexanderBratskov@localhost.localdomain>
2019-06-29 00:18:15 +0300

    added dz17.2

---
+++ 17.2/src/modules/counter.js
@@ -0,0 +1,25 @@
+function getCounterObject() {
+    let counter = 0;
+
+    function increase(slogan) {
+        counter += slogan;
+    }
+    function decrease(slogan) {
+        counter -= slogan;
+    }
+    function show() {
+        console.log(`counter ${counter}`);
+    }
+
+    const obj = { increase, decrease, show }
+
+    return obj;
+}
+
+const objCounter = getCounterObject();
+
+objCounter.increase(321);
+objCounter.decrease(123);
+objCounter.show();
+
+//export { getCounterObject };
---
+++ 17.2/src/modules/sum.js
@@ -0,0 +1,19 @@
+function sum(value) {
+    let result = 0;
+
+    function sum2(slogan) {
+        result += slogan;
+        return sum2;
+    }
+
+    sum2.prototype.showResult = function () {
+        console.log(result);
+    }
+
+    sum2(value)
+    return sum2;
+}
+
+sum(1)(2)(3)(4)(5).showResult();
+
+//export { sum };
