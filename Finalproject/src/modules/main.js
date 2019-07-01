5c3190fba376c65ba0d7e04e7f69a453c7204115
Alexander Bratskov <AlexanderBratskov@localhost.localdomain>
2019-07-01 23:41:35 +0300

    added cycle for

--- Finalproject/src/modules/main.js
+++ Finalproject/src/modules/main.js
@@ -1,4 +1,33 @@
 let nowDate = new Date();
-nowDate.getDate();
-nowDate.setDate(nowDate.getDate() + 1);
-for (let = i)
+
+let today = nowDate.getDate();
+let currentMonth = nowDate.getMonth();
+let currentYear = nowDate.getFullYear();
+let temp = new Date(currentYear, currentMonth, 1, 0)
+let lastDay = temp.getDate();
+
+console.log(lastDay);
+
+
+
+function createTable() {
+    let arrayDate = [];
+    for (let i = 1; i <= 30; i++) {
+        nowDate.setDate(i);
+        let td = document.createElement('td');
+        td.innerHTML = nowDate.getDate();
+        arr.push(td);
+    }
+    for (let i = 1; i < arrayDate.length; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < 7; j++) {
+            tr.appendChild(arrayDate[j])
+        }
+        table.appendChild(tr);
+    }
+}
+
+
+
+
+console.log();
