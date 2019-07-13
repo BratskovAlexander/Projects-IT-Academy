b4cf49b4ad31e4d20f137030f99c105fff810b21
BratskovAlexander <monutor@mail.ru>
2019-07-13 12:50:38 +0300

    added forms

--- Finalproject/src/modules/getListTime.js
+++ Finalproject/src/modules/getListTime.js
@@ -1,31 +1,36 @@
+function addForm() {
+  const getBtnMeeting = document.getElementById("btns-meeting");
+  getBtnMeeting.classList.add("none");
+  getBtnMeeting.innerHTML = "";
+  const form = document.querySelector(".form-meeting");
+  form.classList.remove("none");
+}
+
 function AddBtnWithTime() {
   const infoAboutMe = document.querySelector(
     ".page-info-about div.infoAboutMe"
   );
   infoAboutMe.classList.add("none");
+
   const getBtnMeeting = document.getElementById("btns-meeting");
   getBtnMeeting.classList.remove("none");
+  getBtnMeeting.innerHTML = "";
+
+  const form = document.querySelector(".form-meeting");
+  form.classList.add("none");
 
   let arrayBtnTime = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  let startTimeToBtn = 8;
+  let timeToBtn = 8;
 
   arrayBtnTime.forEach(() => {
     const btnMeeting = document.createElement("button");
+    btnMeeting.addEventListener("click", addForm);
     btnMeeting.classList.add("button-meeting");
     btnMeeting.innerText = `${timeToBtn}.00 - ${timeToBtn + 1}.00`;
     getBtnMeeting.appendChild(btnMeeting);
     timeToBtn++;
   });
-}
-
-const btnTime = document.querySelector("#btns-meeting .button-meeting button");
-if (btnTime != null) {
-  function addForm() {
-    const form = document.querySelector(".form-meeting");
-    form.classList.remove("none");
-    btnTime.addEventListener("click", addForm);
-  }
 }
 
 export { AddBtnWithTime };
