69e150f257c2573244d129a4e600c28518adc360
BratskovAlexander <monutor@mail.ru>
2019-07-23 16:05:39 +0300

    added save in LS and send mail

--- Finalproject/src/modules/addObjectMeeting.js
+++ Finalproject/src/modules/addObjectMeeting.js
@@ -1,37 +1,25 @@
 // import { Calendar } from "./calender.js";
 import { AddBtnWithTime, dateClick } from "./getListTime.js";
 import { addForm, timeClick } from "./addForm.js";
-
+  
   const btnForm = document.querySelector(".btn-form");
   btnForm.addEventListener("click", elem => {
-  elem.preventDefault();
-  let dateTimeClick = dateClick + "/" + timeClick;
+  
+  let dateTimeClick = dateClick + " / " + timeClick;
   let meetingForm = {};
   meetingForm.nameUser = document.querySelector(".name").value;
   meetingForm.email = document.querySelector(".email").value;
   meetingForm.tel = document.querySelector(".tel").value;
   meetingForm.textArea = document.querySelector(".text-area").value;
- 
+  // elem.preventDefault();
+  // JSON.stringify(meetingForm);
 
-  JSON.stringify(meetingForm);
-  localStorage.setItem(
-    JSON.stringify(dateTimeClick),
-    JSON.stringify(meetingForm)
-  );
+  localStorage.setItem(JSON.stringify(dateTimeClick), JSON.stringify(meetingForm));
 
-  let returnObj = JSON.parse(
-    localStorage.getItem(JSON.stringify(dateTimeClick))
-  );
-  
-  if (`"${dateTimeClick}"` === JSON.stringify(dateTimeClick) ) {
-    alert(2)
-    //document.querySelector("button").setAttribute('disabled');
-  }
+  let returnObj = JSON.parse(localStorage.getItem(JSON.stringify(dateTimeClick)));
 
-});
+ });
 
-function addObjectMeeting() {
- 
-}
+function addObjectMeeting() {};
 
 export { addObjectMeeting };
